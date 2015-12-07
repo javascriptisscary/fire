60db685c94237a1af6cce83cbae18e2e88bf242e
Kenneth <crazedpokerpro@yahoo.com>
2015-12-07 18:57:24 -0500

    fixing landscape view with js

--- js.js
+++ js.js
@@ -1,15 +1,32 @@
 "use strict";
 $( document ).ready(function() {
-   var bg = $("#intro").css("background") 
    
    
-    if ($(window).width() < 1100) {
-      $("#intro").css("background", "black no-repeat scroll left top / cover")
-      $("#fire").css("font-size", "10em")
+   function landscape() {
+    if ($(window).height() <=654) {
+       return $("#fire").css("font-size", "4em");
+    }
+   }
    
+   
+   
+   
+   
+   
+
+   
+   
+  if  ($(window).width() < 1100) {
+      $("#intro").css("background", "black no-repeat scroll left top / cover");
+      $("#fire").css("font-size", "10em");
+          if ($(window).width() <= 767) {
+        $("#intro").css("background", "black no-repeat scroll left top / cover");
+      $("#fire").css("font-size", "6em");
+      
+    }
     }
     
     
-    
+    landscape();
     
 });
