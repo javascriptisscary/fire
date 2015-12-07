24aad935f168e6b7522a5e2a602dd188e5046388
Kenneth <crazedpokerpro@yahoo.com>
2015-12-07 16:51:07 -0500

    fix mobile bg img with js

---
+++ js.js
@@ -0,0 +1,15 @@
+"use strict";
+$( document ).ready(function() {
+   var bg = $("#intro").css("background") 
+   
+   
+    if ($(window).width() < 1100) {
+      $("#intro").css("background", "black no-repeat scroll left top / cover")
+      $("#fire").css("font-size", "10em")
+   
+    }
+    
+    
+    
+    
+});
